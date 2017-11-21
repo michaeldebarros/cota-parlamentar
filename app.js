2a1506cc14add1379056734678db99ae47d729e2
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2017-11-21 10:28:51 -0200

    changed the large JSON file for CSV file

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
+const csv = require("fast-csv");
+const fs = require("fs");
 const express = require("express");
 const app = express();
-const fs = require("fs");
 
-const rs = fs.createReadStream("./Ano-2016.json", {
-  encoding: "latin1",
-  start: 0
+
+app.get("/:ano/:numeroCarteiraDeputado/:despesa", (req, res) => {
+  const results = [];
+  const ano = req.params.ano;
+  const numeroCarteiraDeputado = req.params.numeroCarteiraDeputado;
+  const despesa = req.params.despesa;
+  const stream = fs.createReadStream(`Ano-${ano}.csv` , {
+    start: 0,
+    end: 3000000
+  });
+
+  csv
+    .fromStream(stream, {
+      delimiter: ";",
+      headers: [
+        "nome",
+        ,
+        "numeroCarteiraDeputado",
+        ,
+        "estado",
+        "partido",
+        ,
+        ,
+        "tipoDespesa",
+        ,
+        "desricaoEspecifica",
+        "fornecedor",
+        "cnpjFornecedor",
+        ,
+        ,
+        "data",
+        ,
+        ,
+        "valor",
+        ,
+        ,
+        ,
+        ,
+        ,
+        ,
+        ,
+        ,
+        ,
+        ,
+      ]
+    })
+    .on("data", data => {
+      if (data.numeroCarteiraDeputado === numeroCarteiraDeputado) {
+        results.push(data);
+      }
+    })
+    .on("end", () => {
+      res.json(results);
+    });
 });
-
-
-
-app.get("/:ano/:deputado/:despesa/:", (req, res) => {});
-  let ano = req.params.ano;
-  let deputado = req.params.deputado;
-  let despesa = req.params.despesa;
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Express is seving on port ${process.env.PORT || 3000}`);
