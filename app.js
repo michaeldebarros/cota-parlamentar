22394977d7155ea7b4e105850bb67214cf3f572f
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2017-12-20 10:25:53 -0200

    added full path

--- app.js
+++ app.js
@@ -3,25 +3,16 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
-app.get("/", (req, res) => {
-  res.send("Obrigado por usar a API Cota Parlamentar");
-});
-app.get("/:ano/:nome/:despesa", (req, res) => {
-  let stopSign = null;
+app.get("/:ano/:numeroCarteiraDeputado/:despesa", (req, res) => {
   const results = [];
   const ano = req.params.ano;
-  const nome = req.params.nome;
+  const numeroCarteiraDeputado = req.params.numeroCarteiraDeputado;
   const despesa = req.params.despesa;
-  const stream = fs.createReadStream(`Ano-${ano}.csv`, { start: 0 });
+  const stream = fs.createReadStream(`Ano-${ano}.csv` , {
+    start: 0,
+    end: 3000000
+  });
 
   csv
     .fromStream(stream, {
@@ -52,26 +43,22 @@ app.get("/:ano/:nome/:despesa", (req, res) => {
         ,
         ,
         ,
-        "numeroRestituicao",
+        ,
         ,
         ,
         ,
       ]
     })
     .on("data", data => {
-      if (data.nome === nome && data.tipoDespesa === despesa) {
+      if (data.numeroCarteiraDeputado === numeroCarteiraDeputado) {
         results.push(data);
-        stopSign = false;
-      } else if (stopSign === false) {
-        res.json(results);
-        stopSign = true;
       }
     })
     .on("end", () => {
-      if (results.length === 0) res.send("No data found.");
+      res.json(results);
     });
 });
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`Express is seving on port ${process.env.PORT || 3001}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Express is seving on port ${process.env.PORT || 3000}`);
 });
