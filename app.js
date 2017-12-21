4c7299a505c3aeba94868270967e300fe786617b
Michael De Barros <michaeldebarros@MacBook-Air-de-Michael.local>
2017-12-21 14:22:18 -0200

    changed message to be sent if no results show

--- app.js
+++ app.js
@@ -1,89 +1,86 @@
-const csv = require("fast-csv");
-const fs = require("fs");
-const express = require("express");
+const csv = require('fast-csv');
+const fs = require('fs');
+const express = require('express');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
-app.get("/", (req, res) => {
-  res.send("Obrigado por usar a API Cota Parlamentar");
+app.get('/', (req, res) => {
+	res.send('Obrigado por usar a API Cota Parlamentar');
 });
-app.get("/:ano/:nome/:despesa", (req, res, next) => {
-  let stopSign = null;
-  const results = [];
-  const ano = req.params.ano;
-  const nome = req.params.nome;
-  const despesa = req.params.despesa;
-  const stream = fs.createReadStream(`Ano-${ano}.csv`, { start: 0 });
-  console.log(ano, nome, despesa);
+app.get('/:ano/:nome/:despesa', (req, res, next) => {
+	let stopSign = null;
+	const results = [];
+	const ano = req.params.ano;
+	const nome = req.params.nome;
+	const despesa = req.params.despesa;
+	const stream = fs.createReadStream(`Ano-${ano}.csv`, { start: 0 });
 
-  stream.on("error", () => {
-    //throw new Error("there was an error");
-    //res.status(500).send("Internal Error")
-    next(new Error("there was an error"));
-  });
+	stream.on('error', () => {
+		//throw new Error("there was an error");
+		//res.status(500).send("Internal Error")
+		next(new Error('there was an error'));
+	});
 
-  csv
-    .fromStream(stream, {
-      delimiter: ";",
-      headers: [
-        "nome",
-        ,
-        "numeroCarteiraDeputado",
-        ,
-        "estado",
-        "partido",
-        ,
-        ,
-        "tipoDespesa",
-        ,
-        "desricaoEspecifica",
-        "fornecedor",
-        "cnpjFornecedor",
-        ,
-        ,
-        "data",
-        ,
-        ,
-        "valor",
-        ,
-        ,
-        ,
-        ,
-        ,
-        ,
-        "numeroRestituicao",
-        ,
-        ,
-        ,
-      ]
-    })
-    .on("data", data => {
-      if (data.nome === nome && data.tipoDespesa === despesa) {
-        results.push(data);
-        stopSign = false;
-      } else if (stopSign === false) {
-        res.json(results);
-        stopSign = true;
-      }
-    })
-    .on("end", () => {
-      if (results.length === 0) res.send("No data found.");
-    });
+	csv
+		.fromStream(stream, {
+			delimiter: ';',
+			headers: [
+				'nome',
+				,
+				'numeroCarteiraDeputado',
+				,
+				'estado',
+				'partido',
+				,
+				,
+				'tipoDespesa',
+				,
+				'desricaoEspecifica',
+				'fornecedor',
+				'cnpjFornecedor',
+				,
+				,
+				'data',
+				,
+				,
+				'valor',
+				,
+				,
+				,
+				,
+				,
+				,
+				'numeroRestituicao',
+				,
+				,
+				,
+			],
+		})
+		.on('data', data => {
+			if (data.nome === nome && data.tipoDespesa === despesa) {
+				results.push(data);
+				stopSign = false;
+			} else if (stopSign === false) {
+				res.send('Não há dados para essa requisição.');
+				res.json(results);
+				stopSign = true;
+			}
+		})
+		.on('end', () => {
+			if (results.length === 0) res.send('Não há dados para essa requisição.');
+		});
 });
 
 //Error handler
 app.use((err, req, res, next) => {
-  if (err) res.status(500).send("Internal Error sent by the global handler");
+	if (err) res.status(500).send('Internal Error sent by the global handler');
 });
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`Express is seving on port ${process.env.PORT || 3001}`);
+	console.log(`Express is seving on port ${process.env.PORT || 3001}`);
 });
